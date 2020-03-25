d472cb20ab8afe21fba7a564f9c00aaf6eca2143
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-25 10:11:50 -0300

    Create spanish language file

---
+++ src/languages/es.js
@@ -0,0 +1,20 @@
+module.exports = {
+      
+      EMBED_COLOR: '#EE82EE',
+      EMBED_FOOTER_IMAGE: '$botavatar',
+      EMBED_FOOTER_TEXT: 'Bot desarrollado por Pabszito#7777'
+      
+      NO_PERMISSION_USER: ':error: No tienes permisos para ejecutar ese comando',
+      NO_PERMISSION_BOT: ':error: No tengo los permisos suficientes para realizar esa accion.',
+      USAGE: ':error: Uso correcto: $usage',
+      
+      BANNED: ':success: $target fue baneado del servidor.',
+      WARNED: ':success: $target fue advertido.',
+      KICKED: ':success: $target fue kickeado del servidor.',
+      MUTED: ':success: $target fue muteado.',
+      UNMUTED: ':success: $target fue desmuteado.',
+      UNBANNED: ':success: $target fue desbaneado.',
+      UNWARNED: ':success: Se elimino una de las advertencias de $target.',
+      NO_WARNINGS: ':error: $target no tiene advertencias
+      
+}
