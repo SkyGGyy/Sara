d62dc826443aff19c32100015ff847c485444d33
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:20:49 -0300

    Change embed title and owner tag

--- src/commands/hpussy.js
+++ src/commands/hpussy.js
@@ -5,9 +5,9 @@ const Discord = require('discord.js');
 module.exports.run = async(client, message, args) => {
     let pussy = await neko.nsfw.pussy();
     let embed = new Discord.RichEmbed()
-        .setTitle("Aqui esta lo que pedias...")
+        .setTitle("...")
         .setImage(pussy.url)
-        .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL)
+        .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL)
         .setColor("#EE82EE");
     message.channel.send(embed);
 };
