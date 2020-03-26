7c35fc257ee654d5139137d17f3f6068e6831ccb
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:31:35 -0300

    Use utils.json

--- src/commands/pastebin.js
+++ src/commands/pastebin.js
@@ -1,32 +1,34 @@
 const Discord = require('discord.js');
 const PastebinAPI = require('pastebin-js');
 const botConfig = require('../storage/botconfig.json');
-var pastebin = new PastebinAPI(botConfig.pastebinapikey);
+const pastebin = new PastebinAPI(botConfig.pastebinapikey);
 pastebin = new PastebinAPI({
     'api_dev_key': botConfig.pastebinapikey,
     'api_user_name': botConfig.pastebinusername,
     'api_user_password': botConfig.pastebinuserpassword
 })
 
+const utils = require('../utils.json');
+
 module.exports.run = async (client, message, args) => {
     let paste = args.slice(0).join(" ")
     let type = args.slice(1).join(" ")
-    if (!args[0]) {
-        return message.channel.send("<:error:619698101447294977> No puedo subir aire a Pastebin.")
-    }
+    
+    if (!args[0]) return message.channel.send(`${utils.error} Necesitas especiificar algo, ya sea codigo, o un texto comun y corriente..`)
+    
     pastebin
         .createPaste(paste, "Post de " + message.author.tag)
         .then(function (data) {
-            const embed = new Discord.RichEmbed()
+            let embed = new Discord.RichEmbed()
                 .setTitle("Pastebin")
-                .setDescription("Tu post fue creado con exito! Puedes verlo en " + data + " cuando tu quieras.\n Raw: https://pastebin.com/raw/" + data.substring(21));
-            embed.setColor("#EE82EE");
-            embed.setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+                .setDescription("Tu post fue creado con exito! Puedes verlo en " + data + " cuando tu quieras.\n Raw: https://pastebin.com/raw/" + data.substring(21))
+                .setColor("#EE82EE")
+                .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
             message.channel.send(embed);
         })
         .fail(function (err) {
             // Something went wrong
-            console.log(err).then(message.channel.send("<:error:619698101447294977> No se pudo subir lo que especificaste. Puede que https://pastebin.com no se encuentre disponible. Puedes comprobarlo por ti mismo o intentar de nuevo mas tarde."));
+            console.log(err).then(message.channel.send(`${utils.erorr} No se pudo subir lo que especificaste. Puede que https://pastebin.com no se encuentre disponible. Puedes comprobarlo por ti mismo o intentar de nuevo mas tarde.`));
         })
 }
 
