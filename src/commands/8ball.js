f709809daad2faa9f42ffb0d83ee960fa5be828a
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-25 23:54:46 -0300

    Update owner tag, remove spaninglish

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -4,15 +4,15 @@ module.exports.run = async (client, message, args) => {
 
     if (!args[0]) return message.channel.send("<:error:619698101447294977> Debes especificar una pregunta");
 
-    const respuestas = ["En mi opinión, sí", "Es cierto", "Es decididamente así", "Probablemente", "Buen pronóstico", "Todo apunta a que sí", "Sin duda", "Sí", "Definitivamente", "Debes confiar en ello", "Respuesta vaga, vuelve a intentarlo", "Pregunta en otro momento", "Será mejor que no te lo diga ahora", "No puedo predecirlo ahora", "Concéntrate y vuelve a preguntar", "Puede ser", "No cuentes con ello", "Mi respuesta es no", "Mis fuentes me dicen que no", "Las perspectivas no son buenas", "Muy dudoso", "Sabes? 9 de 10 odontologos recomiendan que te calles..."]; // recien copia3 del bot de viruscrafters
+    let responses = ["En mi opinión, sí", "Es cierto", "Es decididamente así", "Probablemente", "Buen pronóstico", "Todo apunta a que sí", "Sin duda", "Sí", "Definitivamente", "Debes confiar en ello", "Respuesta vaga, vuelve a intentarlo", "Pregunta en otro momento", "Será mejor que no te lo diga ahora", "No puedo predecirlo ahora", "Concéntrate y vuelve a preguntar", "Puede ser", "No cuentes con ello", "Mi respuesta es no", "Mis fuentes me dicen que no", "Las perspectivas no son buenas", "Muy dudoso", "Sabes? 9 de 10 odontologos recomiendan que te calles..."];
 
-    let resultado = Math.floor((Math.random() * respuestas.length));
+    let result = Math.floor((Math.random() * responses.length));
 
-    const embed = new Discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
         .setTitle("🎱 8ball")
-        .setDescription(`Mi respuesta es ${resultado}`)
+        .setDescription(`Mi respuesta es ${result}`)
         .setColor("#EE82EE")
-        .setFooter('Bot desarrollado por Pabszito#7790', client.user.avatarURL); // footer
+        .setFooter('Bot desarrollado por Pabszito#7777', client.user.avatarURL); 
     message.channel.send(embed);
 }
 
