8cde1c06ee20f78bb05b4cd4073ee8e50d72b190
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:20:23 -0300

    Change embed title and owner tag

--- src/commands/hentai.js
+++ src/commands/hentai.js
@@ -8,10 +8,10 @@ module.exports.run = async (client, message, args) => {
     if (message.channel.nsfw === true) {
         let hentai = await neko.nsfw.hentai();
         let embed = new Discord.RichEmbed()
-            .setTitle("Aqui esta lo que pedias...")
+            .setTitle("...")
             .setImage(hentai.url)
             .setColor("#EE82EE")
-            .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+            .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
         message.channel.send(embed);
     } else {
         message.channel.send(`${utils.error} Debes estar en un canal NSFW para utilizar ese comando!`)
