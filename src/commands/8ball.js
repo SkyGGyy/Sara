b4940d68a0b4cad1e32a8a9a7d4e6729175c29e4
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:58:00 -0300

    Use utils

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const utils = require('../utils.json');
 
 module.exports.run = async (client, message, args) => {
 
-    if (!args[0]) return message.channel.send("<:error:619698101447294977> Debes especificar una pregunta");
+    if (!args[0]) return message.channel.send(`${utils.error} Debes hacer una pregunta.`);
 
     let responses = ["En mi opinión, sí", "Es cierto", "Es decididamente así", "Probablemente", "Buen pronóstico", "Todo apunta a que sí", "Sin duda", "Sí", "Definitivamente", "Debes confiar en ello", "Respuesta vaga, vuelve a intentarlo", "Pregunta en otro momento", "Será mejor que no te lo diga ahora", "No puedo predecirlo ahora", "Concéntrate y vuelve a preguntar", "Puede ser", "No cuentes con ello", "Mi respuesta es no", "Mis fuentes me dicen que no", "Las perspectivas no son buenas", "Muy dudoso", "Sabes? 9 de 10 odontologos recomiendan que te calles..."];
 
@@ -13,6 +14,7 @@ module.exports.run = async (client, message, args) => {
         .setDescription(`Mi respuesta es ${result}`)
         .setColor("#EE82EE")
         .setFooter('Bot desarrollado por Pabszito#7777', client.user.avatarURL); 
+        
     message.channel.send(embed);
 }
 
