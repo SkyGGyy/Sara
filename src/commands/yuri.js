30acbd0483adde9eeb2f481bb3b8795790659f03
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:38:41 -0300

    Update embed title and owner tag

--- src/commands/yuri.js
+++ src/commands/yuri.js
@@ -7,10 +7,10 @@ module.exports.run = async (client, message, args) => {
     if (message.channel.nsfw === true) {
         let yuriimg = await neko.nsfw.yuri();
         let embed = new Discord.RichEmbed()
-            .setTitle("Aqui esta lo que pedias...")
+            .setTitle("...")
             .setImage(yuriimg.url)
             .setColor("#EE82EE")
-            .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+            .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
         message.channel.send(embed)
     } else {
         message.channel.send(`${utils.error} Debes estar en un canal NSFW para utilizar ese comando!`)
