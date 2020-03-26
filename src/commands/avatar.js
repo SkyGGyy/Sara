e5059f6d97ac73851cc3c7963fca8ab9064fa22c
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:59:56 -0300

    Fix avatar command

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -6,11 +6,11 @@ module.exports.run = async (client, message, args) => {
 
     let embed = new Discord.RichEmbed()
         .setTitle("Avatar")
-        .setDescription(`${target === message.author ? "Aqui tienes tu avatar" : "Aqui esta el avatar de ${message.author.tag}.`)
-        .setImage(user.avatarURL);
+        .setDescription(`${target === message.author ? "Aqui tienes tu avatar" : "Aqui esta el avatar de ${message.author.tag}."}`)
+        .setImage(user.avatarURL)
         .setColor("#EE82EE")
         .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL)
-    message.channel.send({embed});
+    message.channel.send(embed);
 }
 module.exports.help = {
     name: "avatar"
