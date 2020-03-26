c18aed43f37ff41f6d2a12e004aac6e98a3968d8
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:14:25 -0300

    Update command
    Replace const for let in internal blocks, use utils, fix embed, and added a message before submitting.

--- src/commands/hastebin.js
+++ src/commands/hastebin.js
@@ -1,20 +1,24 @@
 const Discord = require('discord.js');
 const hastebin = require('hastebin-gen');
+const utils = require('../utils.json');
 
 module.exports.run = (client, message, args) => {
     let haste = args.slice(0).join(" ")
     let type = args.slice(1).join(" ")
     if (!args[0]) {
-        return message.channel.send("<:error:619698101447294977> No puedo subir aire a Hastebin.")
+        return message.channel.send(`${utils.error} Debes especificar algo, ya sea codigo o texto.`)
     }
+    
+    message.channel.send(`${utils.info} Subiendo, si nunca se obtiene una respuesta es porque Hastebin no se encuentra disponible.`);
+    
     hastebin(haste).then(r => {
-        const embed = new Discord.RichEmbed()
+        let embed = new Discord.RichEmbed()
             .setTitle("Hastebin")
-            .setDescription("Tu post fue creado con exito! Puedes verlo en " + r + " cuando tu quieras.");
-        embed.setColor("#EE82EE");
-        embed.setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+            .setDescription(`Tu post fue creado con exito! Puedes verlo en ${r} cuando tu quieras.`)
+            .setColor("#EE82EE")
+            .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
         message.channel.send(embed);
-    }).catch(console.error).then(message.channel.send("<:error:619698101447294977> No se pudo subir lo que especificaste. Puede que https://hastebin.com no se encuentre disponible. Puedes comprobarlo por ti mismo o intentar de nuevo mas tarde."));
+    });
 }
 
 module.exports.help = {
