3064fd2dc29b8c3fd58798b222ceb7c3fc9e32cf
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:23:21 -0300

    Recode pending; use await

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,28 +1,23 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-    let user = message.mentions.users.first();
+    let target = message.mentions.users.first();
     let razon = args.slice(1).join(' ');
     const perms = message.member.hasPermission("KICK_MEMBERS");
 
     if (!perms) return message.channel.send("<:error:619698101447294977> Permisos insuficientes.");
     if (message.mentions.users.size < 1) return message.channel.send('<:error:619698101447294977> Debes mencionar a alguien.').catch(console.error);
     if (!razon) return message.channel.send('<:error:619698101447294977> Debes especificar una razon.');
-    if (!message.guild.member(user).kickable) return message.channel.send('<:error:619698101447294977> No puedo kickear al usuario mencionado.');
+    if (!message.guild.member(target).kickable) return message.channel.send('<:error:619698101447294977> No puedo kickear al usuario mencionado.');
 
-    user.send("**Fuiste kickeado!**\n" +
+    await target.send("**Fuiste kickeado!**\n" +
         `Staff: ${message.author}\n` +
         `Razon: ${razon} \n` +
         `Servidor: ${message.guild.name}`).catch(console.error);
 
-
-    function continueKick() {
-        message.guild.member(user).kick(razon);
-    }
-
-    setTimeout(continueKick, 1000);
-
-    const embed = new Discord.RichEmbed()
+    message.guild.member(user).kick(razon);
+    
+    let embed = new Discord.RichEmbed()
         .setTitle("Usuario kickeado")
         .setDescription('Un usuario ha sido kickeado!')
         .addField("Staff:", message.author, false)
