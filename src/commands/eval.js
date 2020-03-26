3f6bd7e781e3424a2bcd028e45630c099976eb6c
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:05:22 -0300

    Change const for let in internal blocks x999999999

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
 
     if (message.author.id === '447902653842980875') {
-        const command = message.content.split(' ').slice(1).join(' ');
+        let command = message.content.split(' ').slice(1).join(' ');
         message.channel.send(`${eval(command)}`);
     }
 }
