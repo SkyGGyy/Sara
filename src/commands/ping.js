270dc0a2979c3c1a47001eb228e288b3fe019de5
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:32:40 -0300

    Replace const for let in internal blocks

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-    const embed = new Discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
         .setTitle("Ping")
-        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde glitch.");
-    embed.setColor("#EE82EE");
-    embed.setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
-    message.channel.send({embed});
+        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde glitch.")
+        .setColor("#EE82EE")
+        .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+    message.channel.send(embed);
 }
 
 module.exports.help = {
