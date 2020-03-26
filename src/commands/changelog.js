96a081532afa7d0c3123cdb99a1e174a62b15aa3
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 03:36:34 -0300

    Create changelog command

---
+++ src/commands/changelog.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = async(client, message, args) =>{
+
+    let embed = new Discord.RichEmbed()
+    .setTitle("Lista de cambios")
+    .setDescription("La lista de cambios fue implementada en la version 2.4.1 por lo que se mostraran los cambios a partir de esa version.")
+    .addField("2.4.1", "- Se optimizo el codigo de Sara.\n- Se añadio el comando changelog\n- Se añadieron de nuevo los comandos de musica.\n"+
+    "- Se eliminaron algunos comandos innecesarios.\n- Se mejoraron algunos comandos como kiss, hug, triggered y pat.\n- El comando hastebin ya no mostrara un mensaje diciendo que no se pudo subir lo especificado.\n"+
+    "- Se elimino el comando teamtrees\n- Se actualizo el tag de Pabszito#7790 a Pabszito#7777.\n- Entre muchos otros cambios internos")
+    .setColor("#EE82EE")
+    .addField("Errores?", "Has encontrado un error? Recuerda que puedes reportarlo [aqui](https://github.com/Pabszito/Sara/issues)! Tambien puedes ayudarnos a solucionarlo haciendo un [pull request](https://github.com/Pabszito/Sara/pulls) en el repo de GitHub.")
+    .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL)
+
+    message.channel.send(embed);
+}
