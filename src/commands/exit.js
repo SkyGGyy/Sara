b36b75d47df93a1bf2b4ce88f097af131f41d0ca
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:05:45 -0300

    Remove exit command

--- src/commands/exit.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (client, message, args) => {
-    if (message.author.id === "447902653842980875") {
-        message.channel.send("<:info:619693194862657566> Apagando el bot, por favor espere...").then(client.destroy());
-    }
-}
-
-module.exports.help = {
-    name: "exit"
-}
