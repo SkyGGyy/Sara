164555125709da9d373dff5b1296da63c268293c
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:28:13 -0300

    Update embed title and owner tag

--- src/commands/neko.js
+++ src/commands/neko.js
@@ -7,10 +7,10 @@ module.exports.run = async (client, message, args) => {
     if (message.channel.nsfw === true) {
         let nekoimg = await neko.nsfw.neko();
         let embed = new Discord.RichEmbed()
-            .setTitle("Aqui esta lo que pedias...")
+            .setTitle("...")
             .setImage(nekoimg.url)
             .setColor("#EE82EE")
-            .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+            .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
         message.channel.send(embed)
     } else {
         message.channel.send(`${utils.error} Debes estar en un canal NSFW para utilizar ese comando!`)
