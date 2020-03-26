886ed13386b92bb44db99362cdbcbf8c8ad860f0
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:02:00 -0300

    Remove embed description and update owner tag
    Also replaced const for let in internal blocks

--- src/commands/cat.js
+++ src/commands/cat.js
@@ -4,12 +4,11 @@ const request = require('request');
 module.exports.run = async (client, message, args) => {
     request('http://edgecats.net/random', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            const embed = new Discord.RichEmbed()
+            let embed = new Discord.RichEmbed()
                 .setTitle("meow!")
-                .setDescription("Ten un gatito.. OwO")
                 .setImage(body);
             embed.setColor("#EE82EE");
-            embed.setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+            embed.setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
             message.channel.send(embed);
         }
     });
