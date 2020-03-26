a0259d6b3bd2e5a4c16ede2397256448861795d5
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-03-26 00:27:24 -0300

    Replace const for let in internal blocks

--- src/commands/morse.js
+++ src/commands/morse.js
@@ -5,7 +5,7 @@ const utils = require('../utils.json')
 module.exports.run = async (client, message, args) => {
     if (!args[0]) return message.channel.send(`${utils.error} Necesitas especificar un texto!`);
     let texto = args.slice(0).join(' ');
-    const translated = morse.encode(texto);
+    let translated = morse.encode(texto);
     let embed = new Discord.RichEmbed()
         .setTitle('Traductor morse')
         .setDescription('Tu texto fue traducido! Estos son los resultados:\n**Texto original:** ' + texto + "\n**Traducido a morse:** " + translated)
